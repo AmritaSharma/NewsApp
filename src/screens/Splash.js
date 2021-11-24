27f9968c1dbec291dbd32f6f4094d711097dd63c
amars2905 <amardeepsen1111@gmail.com>
2021-11-24 14:16:54 +0530

    fix theme color

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { View, Text } from "react-native"
+import React from 'react';
+import {View, Text} from 'react-native';
+import {AppColors} from '../constants/appColors';
 
 const Splash = ({navigation}) => {
   return (
     <View>
-      <Text>Splash</Text>
+      <Text style={{color: AppColors().accent}}>Splash</Text>
     </View>
   );
 };
